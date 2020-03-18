cff588c8b3ab46d44b0c3a2b638cb79da6d4cb73
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-03-18 12:11:45 +0530

    kakfa project

---
+++ src/main/java/com/khs/kafka/KafkaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.khs.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khs/kafka/KafkaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khs.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
