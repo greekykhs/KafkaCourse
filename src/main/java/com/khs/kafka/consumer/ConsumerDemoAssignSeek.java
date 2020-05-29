d20c4d7ef7f713869fe307115f25f45505e6c864
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-05-29 12:53:37 +0530

    ConsumerDemoAssignSeek updated

--- src/main/java/com/khs/kafka/consumer/ConsumerDemoAssignSeek.java
+++ src/main/java/com/khs/kafka/consumer/ConsumerDemoAssignSeek.java
@@ -16,54 +16,56 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ConsumerDemoAssignSeek {
-	public static void main(String[] args) {
-		Logger logger=LoggerFactory.getLogger(ConsumerDemoAssignSeek.class.getName());
-		
-		String BOOT_STRAP_SERVER="127.0.0.1:9092";
-		String groupId="my-fourth-application";
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignSeek.class.getName());
+        String BOOT_STRAP_SERVER="127.0.0.1:9092";
 		String TOPIC_NAME="first_topic";		
-		
-		//Create consumer configs
-		Properties properties=new Properties();		
-		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, 
-				BOOT_STRAP_SERVER);
-		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, 
-				StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, 
-				StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, 
-				groupId);
-//		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, 
-//				"earliest");
-		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, 
-				"latest");
-//		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, 
-//				"none");
-		
-		//create consumer
-		KafkaConsumer<String, String> consumer=new KafkaConsumer<>(properties);
-		
-		//subscribe consumer to our topics
-		//consumer.subscribe(Collections.singleton(TOPIC_NAME));
-		consumer.subscribe(Arrays.asList(TOPIC_NAME));
-		
-		ConsumerRecords<String, String> records;
-		
-		//poll for new data
-		while(true) {
-			//new in Kafka 2.0.0
-			//consumer.poll(100);
-			
-			records=consumer.poll(Duration.ofMillis(100));
-			
-			for(ConsumerRecord<String, String> record:records) {
-				logger.info("Key:"+record.key()+"\n"+
+       
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOT_STRAP_SERVER);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // assign and seek are mostly used to replay data or fetch a specific message
+
+        // assign
+        TopicPartition partitionToReadFrom = new TopicPartition(TOPIC_NAME, 0);
+        
+        long offsetToReadFrom = 15L;//start reading from offset 15
+        consumer.assign(Arrays.asList(partitionToReadFrom));
+
+        // seek
+        consumer.seek(partitionToReadFrom, offsetToReadFrom);
+
+        int numberOfMessagesToRead = 5;
+        boolean keepOnReading = true;
+        int numberOfMessagesReadSoFar = 0;
+
+        // poll for new data
+        while(keepOnReading){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+            for (ConsumerRecord<String, String> record : records){
+                numberOfMessagesReadSoFar += 1;
+                logger.info("Key:"+record.key()+"\n"+
 						"Value:"+record.value()+"\n"+
 						"Partition:"+record.partition()+"\n"+
 						"Offsets:"+record.offset());
-			}
-		}		
-	}
+                if (numberOfMessagesReadSoFar >= numberOfMessagesToRead){
+                    keepOnReading = false; // to exit the while loop
+                    break; // to exit the for loop
+                }
+            }
+        }
+
+        logger.info("Exiting the application");
+    }
 	public static void testAssign(String topic, KafkaConsumer<String, String> consumer,
 			Collection<TopicPartition> partitions) {
 		
